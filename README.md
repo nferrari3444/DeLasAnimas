d16fdf9d6db9cfd23cb0cfbb3116019d3dd39d09
nferrari3444 <nferrari3444@gmail.com>
2022-05-01 23:47:29 -0300

    match master with main

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DeLasAnimas
 Landing Page with Payment Processor to purchase Oil Virgin. Communicate about company, it history and features.
+https://fir-hosting-ninja-281e7.web.app
---
+++ public/js/scripts.js
@@ -0,0 +1,60 @@
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY != 0) {
+            navbarCollapsible.classList.add('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 52,
+        });
+    }; 
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+// var shiftWindow = function() {document.getElementById('mainNav').scrollBy(0, -300)}
+
+ 
+
+// const navLinks = document.querySelectorAll('.nav-link')
+// navLinks.forEach(function(link){
+//     link.addEventListener('click', shiftWindow)
+
+
+// })
+
